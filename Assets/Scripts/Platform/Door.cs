184a16dcfd388a915622a24ee81d654aee0d5370
Zihan Gao <gao_zihan@126.com>
2014-01-26 00:52:39 -0800

    switch and door scripting

---
+++ Assets/Scripts/Platform/Door.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    public GameObject[] switches;
+    public float moveOffset = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+
+        for (int i = 0; i < switches.Length; i++)
+        {
+            switches[i].GetComponent<Switch>().door = gameObject;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void CheckCondition()
+    {
+        if (FitsCondition())
+        {
+            Open();
+        }
+    }
+
+    bool FitsCondition()
+    {
+        for (int i = 0; i < switches.Length; i++)
+        {
+            if (switches[i].GetComponent<Switch>().state == Switch.SwitchState.Off)
+                return false;
+        }
+        return true;
+    }
+
+    void Open ()
+    {
+        print("Open");
+        transform.Translate(new Vector3(0f, moveOffset, 0f));
+    }
+
+}
---
+++ Assets/Scripts/Platform/Switch.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+    public enum SwitchState
+    {
+        On,
+        Off
+    }
+
+    public Sprite switchOn;
+    private SpriteRenderer sr;
+    public SwitchState state = SwitchState.Off;
+    public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.gameObject.tag == "Player")
+        {
+            sr.sprite = switchOn;
+            state = SwitchState.On;
+            door.SendMessage("CheckCondition");
+        }
+    }
+
+}
